46d478c46edd54201c07f912e39958e1b5079264
Kai-Smth <166443646+Kai-Smth@users.noreply.github.com>
2024-07-09 09:12:50 -0400

    Update Extraction.java

--- src/main/java/com/codedifferently/labs/partB/ex04/Extraction.java
+++ src/main/java/com/codedifferently/labs/partB/ex04/Extraction.java
@@ -6,12 +6,16 @@ public class Extraction {
         String response = "";
         String greeting = "Learning Java is a good time!";
 
-        /* Your code goes here*/
-
+        // Extracting substrings
+        String substring1 = greeting.substring(0, 5);    
+        String substring2 = greeting.substring(7, 11);   
+        String substring3 = greeting.substring(7);      
+        // Constructing the response
+        response = substring1 + "\n" + substring2 + "\n" + substring3;
 
         return response;
-
     }
+
     public static void main(String[] args) {
         String extractionOutput = extraction();
         System.out.println(extractionOutput);
