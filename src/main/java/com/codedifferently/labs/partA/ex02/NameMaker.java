9ff7ee19bcb9dfb0c5cb90a0bb015a6f5f3fa219
Kai-Smth <166443646+Kai-Smth@users.noreply.github.com>
2024-07-09 09:10:23 -0400

    Update NameMaker.java

--- src/main/java/com/codedifferently/labs/partA/ex02/NameMaker.java
+++ src/main/java/com/codedifferently/labs/partA/ex02/NameMaker.java
@@ -6,8 +6,8 @@ public class NameMaker {
     public static String nameMaker(String firstName, String middleName, String lastName) {
         String response = "";
         String fullName;
-        fullName=firstName+" "+middleName+" "+lastName;
-        response += ("Your full name is "+ fullName);
+        fullName = firstName + " " + middleName + " " + lastName;
+        response += ("Your full name is " + fullName);
         return response;
     }
 
@@ -17,9 +17,23 @@ public class NameMaker {
         String lastName = "";
         /* Put your code in between these comments : Top */
 
+        // Create a Scanner object for reading input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user to enter first name
+        System.out.print("Enter your first name: ");
+        firstName = scanner.nextLine();
+
+        // Prompt user to enter middle name
+        System.out.print("Enter your middle name: ");
+        middleName = scanner.nextLine();
+
+        // Prompt user to enter last name
+        System.out.print("Enter your last name: ");
+        lastName = scanner.nextLine();
 
         /* Put your code in between these comments : Bottom */
-        String fullName  = nameMaker(firstName, middleName, lastName);
+        String fullName = nameMaker(firstName, middleName, lastName);
         System.out.println(fullName);
     }
 }
