a07f0b15557b8476053998461973b839af0edbb4
Kai-Smth <166443646+Kai-Smth@users.noreply.github.com>
2024-07-09 09:15:50 -0400

    Update Replace.java

--- src/main/java/com/codedifferently/labs/partB/ex03/Replace.java
+++ src/main/java/com/codedifferently/labs/partB/ex03/Replace.java
@@ -4,13 +4,13 @@ public class Replace {
     public static String sentence(){
         String response = "";
         String sentence = "Using String replace to replace character";
-        /** Your code goes here*/
-
-
+        String updatedSentence = sentence.replace("r", "R");
+        response = updatedSentence;
         return response;
     }
+    
     public static void main(String[] args) {
         String sentenceOutput = sentence();
         System.out.println(sentenceOutput);
-    }
+    } 
 }
